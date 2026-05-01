50df1a05a7c11695f280f0e8bef68017668f8ef0
lewisbarker06 <lewisbarker06@hotmail.co.uk>
2026-05-01 15:42:20 +0100

    Added testing for address class

---
+++ Testing5/tstCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsAddress AnAddress = new clsAddress();
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
