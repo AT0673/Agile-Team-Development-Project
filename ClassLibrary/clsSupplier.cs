edf64639a7d03c117435c9592ef2e4928f445d40
P2871087 <P2871087@my365.dmu.ac.uk>
2026-05-05 15:16:21 +0100

    added properties for supplier class

---
+++ ClassLibrary/clsSupplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsSupplier
+    {
+        public int SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierEmail { get; set; }
+        public string SupplierPhoneNumber { get; set; }
+        public string SupplierAddress { get; set; }
+        public DateTime SupplierCreatedDate { get; set; }
+        public bool SupplierActive { get; set; }
+
+    }
+}
