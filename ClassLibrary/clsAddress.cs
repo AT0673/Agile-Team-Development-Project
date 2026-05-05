06bae556c2eeb556b57c2087b6f855a82859e7e8
lewisbarker06 <lewisbarker06@hotmail.co.uk>
2026-05-05 15:08:23 +0100

    Added clsAddress

---
+++ ClassLibrary/clsAddress.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ClassLibrary
+{
+
+    public class clsAddress
+    {
+        public bool Active { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public int AddressId { get; set; }
+
+        public int CountyCode { get; set; }
+
+        public string HouseNo { get; set; }
+
+        public string PostCode { get; set; }
+
+        public string Street { get; set; }
+
+        public string Town { get; set; }
+    }
+}
