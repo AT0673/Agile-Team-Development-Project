741b1a37c3d119be63ae2c93e3abdf542762e220
Alfie T. <alfietones0673@outlook.com>
2026-05-06 20:02:18 +0100

    Add clsOrder data entry and viewer, update to .NET 4.8
    Implemented clsOrder capture in data entry page and display in viewer. Updated Web.config to fix merge error, should work fine now. Cleaned up OrderViewer.aspx markup.

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
@@ -14,7 +15,13 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
-        //navigate to the viwer page
+        //create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //capture the order number
+        AnOrder.OrderID = Convert.ToInt32(txtOrderID.Text);
+        //capture the customer ID
+        AnOrder.CustomerID = Convert.ToInt32(txtCustomerID.Text);
+        //navigate to the viewer page
         Response.Redirect("OrderViewer.aspx");
     }
 }
