048ee728c52b4294ceec9d53336b98ddab0b68f1
Joshua Moody <joshuamoody05@gmail.com>
2026-05-14 15:41:27 +0100

    Update product validation, tests, and order status property
    - Added Valid method to clsProduct for input validation.
    - Added extensive unit tests for product validation logic.
    - Renamed Status to OrderStatus in order-related code.
    - Removed Find button from SupplierDataEntry.aspx.
    - Minor formatting and whitespace improvements.

--- ClassLibrary/clsProduct.cs
+++ ClassLibrary/clsProduct.cs
@@ -1,4 +1,4 @@
-﻿using System;  
+﻿using System;
 
 namespace ClassLibrary
 {
@@ -76,13 +76,13 @@ namespace ClassLibrary
             {
                 mProductID = value;
             }
-        } 
+        }
 
         public bool Find(int productID)
         {
             clsDataConnection DB = new clsDataConnection();
-                
-            DB.AddParameter("@ProductID", productID);  
+
+            DB.AddParameter("@ProductID", productID);
 
             DB.Execute("sproc_tblProduct_FilterByProductID");
 
@@ -100,6 +100,84 @@ namespace ClassLibrary
             {
                 return false;
             }
+        }
+
+        public string Valid(string productName, string productAmount, string stockArrivalDate, string supplierID)
+        {
+            String Error = "";
+            DateTime DateTemp;
+
+            //Product Name validation
+            if (productName.Length == 0)
+            {
+                Error = Error + "The product name may not be blank : ";
+            }
+            else if (productName.Length > 50)
+            {
+                Error = Error + "The product name must be less than 50 characters : ";
+            }
+
+            //Product Amount validation
+            if (productAmount.Length == 0)
+            {
+                Error = Error + "The product amount may not be blank : ";
+            }
+            else
+            {
+                int amount;
+                if (!Int32.TryParse(productAmount, out amount))
+                {
+                    Error = Error + "The product amount must be a number : ";
+                }
+                else if (amount <= 0)
+                {
+                    Error = Error + "The product amount must be greater than 0 : ";
+                }
+            }
+
+
+            //Stock Arrival Date validation
+            if (stockArrivalDate.Length == 0)
+            {
+                Error = Error + "The stock arrival date may not be blank : ";
+            }
+            else if (!DateTime.TryParse(stockArrivalDate, out DateTemp))
+            {
+                Error = Error + "The stock arrival date is not a valid date : ";
+            }
+            else
+            {
+                // Use date-only comparisons to avoid time-of-day flakiness
+                DateTime dateOnly = DateTemp.Date;
+                DateTime min = DateTime.Today.AddDays(-30);
+                DateTime max = DateTime.Today.AddDays(30);
+
+                if (dateOnly < min || dateOnly > max)
+                {
+                    Error = Error + "The stock arrival date must be within 30 days of today : ";
+                }
+            }
+
+
+            //Supplier ID validation
+            if (supplierID.Length == 0)
+            {
+                Error = Error + "The supplier ID may not be blank : ";
+            }
+            else
+            {
+                int id;
+                if (!Int32.TryParse(supplierID, out id))
+                {
+                    Error = Error + "The supplier ID must be a number : ";
+                }
+                else if (id <= 0)
+                {
+                    Error = Error + "The supplier ID must be greater than 0 : ";
+                }
+            }
+
+            return Error;
         }
     }
 }
