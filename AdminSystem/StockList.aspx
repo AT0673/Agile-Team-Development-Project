96a5627cbe99c50623bf123455bde9199dfe5e66
Joshua Moody <joshuamoody05@gmail.com>
2026-05-15 14:46:30 +0100

    Add clsProductCollection and product list UI integration
    Introduced clsProductCollection to manage products and fetch from DB.
    Added unit tests for collection functionality. Updated StockList.aspx
    and StockViewer.aspx to display products in a ListBox. Modified
    StockList.aspx.cs to bind product data on page load. Updated project
    files to include new class and test files.

--- AdminSystem/StockList.aspx
+++ AdminSystem/StockList.aspx
@@ -9,6 +9,7 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:ListBox ID="lstProductList" runat="server" Height="389px" Width="304px"></asp:ListBox>
         </div>
     </form>
 </body>
---
+++ ClassLibrary/clsProductCollection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary;
+
+
+namespace ClassLibrary
+{
+    public class clsProductCollection
+    {
+        public clsProductCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblProduct_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsProduct AProduct = new clsProduct();
+                AProduct.ProductID = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductID"]);
+                AProduct.ProductName = Convert.ToString(DB.DataTable.Rows[Index]["ProductName"]);
+                AProduct.ProductAmount = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductAmount"]);
+                AProduct.SupplierID = Convert.ToInt32(DB.DataTable.Rows[Index]["SupplierID"]);
+                AProduct.InStock = Convert.ToBoolean(DB.DataTable.Rows[Index]["InStock"]);
+                AProduct.StockArrivalDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["StockArrivalDate"]);
+                mProductList.Add(AProduct);
+                Index++;
+            }
+        }
+        List<clsProduct> mProductList = new List<clsProduct>();
+        public List<clsProduct> ProductList
+        {             get
+            {
+                return mProductList;
+            }
+            set
+            {
+                mProductList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mProductList.Count;
+            }
+            set
+            {
+                // This setter is not necessary as Count is derived from the list, but it's included for testing purposes.
+            }
+        }
+        public clsProduct ThisProduct { get; set; }
+    }
+}
