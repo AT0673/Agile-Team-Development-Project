fcd586a5190077d6944c1a61a4ad47ee47e2a358
Alfie Tones <alfietones0673@outlook.com>
2026-05-19 00:24:21 +0100

    Add clsOrderUser class and related tests
    Added a new `clsOrderUser` class to the `ClassLibrary` project, including properties for `UserID`, `Username`, `Password`, and `Department`, as well as a `FindUser` method for validating user credentials. Updated `ClassLibrary.csproj` to include the new file.
    
    Added a new test class `tstOrderUser` to the `TestingOrder` project, with unit tests for `clsOrderUser` properties and the `FindUser` method. Updated `TestingOrder.csproj` to include the new test file.
    
    Modified `clsOrderStatus` to add `UserName` and `Username` properties, though the redundancy may need review. Made minor formatting changes to `<Project>` tags in both `.csproj` files.

---
+++ ClassLibrary/clsOrderUser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ClassLibrary
+{
+    public class clsOrderUser
+    {
+        private Int32 mUserID;
+        private String mUsername;
+        private String mPassword;
+        private String mDepartment;
+
+        public string Username
+        {
+            get { return mUsername; }
+            set { mUsername = value; }
+        }
+
+        public int UserID
+        {
+            get {  return mUserID; }
+            set { mUserID = value; }
+        }
+        public string Password
+        {
+            get { return mPassword; }
+            set { mPassword = value; }
+        }
+        public string Department
+        {
+            get { return mDepartment; }
+            set { mDepartment = value; }
+        }
+
+        public bool FindUser(string username, string password)
+        {
+            //Create an instance
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameters using lowercase so it uses the passed arguments
+            DB.AddParameter("@Username", username);
+            DB.AddParameter ("@Password", password);
+            //Excecute the stored procedure
+            DB.Execute("sproc_tblUsers_FindUsernamePW");
+            //if a record exists (there should be one or none)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data memebers
+                mUserID = Convert.ToInt32(DB.DataTable.Rows[0]["UserID"]);
+                mUsername = Convert.ToString(DB.DataTable.Rows[0]["Username"]);
+                mPassword = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                mDepartment = Convert.ToString(DB.DataTable.Rows[0]["Department"]);
+                //Return true if confirmed
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Testing3/tstOrderUser.cs
@@ -0,0 +1,111 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstOrderUser
+    {
+        public object AnUser { get; private set; }
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // create an instance of the class
+            clsOrderUser AnUser = new clsOrderUser();
+            //test to see it isnt null
+            Assert.IsNotNull(AnUser);
+        }
+
+        [TestMethod]
+        public void UserIDPropertyOK()
+        {
+            //Create an instance
+            clsOrderUser AnUser = new clsOrderUser();
+            //Create test data
+            Int32 TestData = 1;
+            //Assign it
+            AnUser.UserID = TestData;
+            //Test to see that theyre equal
+            Assert.AreEqual(AnUser.UserID, TestData);
+        }
+
+        public void UserNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderStatus AnUser = new clsOrderStatus();
+            //create some test data to assign to the property
+            string TestData = "Dawn";
+            //assign the data to the property
+            AnUser.Username = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.Username, TestData);
+        }
+        
+        [TestMethod]
+        public void PasswordPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderUser AnUser = new clsOrderUser();
+            //create some test data to assign to the property
+            string TestData = "password123";
+            //assign the data to the property
+            AnUser.Password = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.Password, TestData);
+        }
+
+        [TestMethod]
+        public void DepartmentPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderUser AnUser = new clsOrderUser();
+            //create some test data to assign to the property
+            string TestData = "Address Book";
+            //assign the data to the property
+            AnUser.Department = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.Department, TestData);
+        }
+
+        [TestMethod]
+        public void FindUserMethodOK()
+        {
+            //create an instance
+            clsOrderUser AnUser = new clsOrderUser();
+            //create a boolean variable
+            Boolean Found = false;
+            //Create test data
+            string Username = "Alfie";
+            string Password = "Password123";
+            //Invoke the method
+            Found = AnUser.FindUser(Username, Password);
+            //Test to see if result is true
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestUsernamePWFound()
+        {
+            //create an instance
+            clsOrderUser AnUser = new clsOrderUser();
+            //create a boolean value to store the result
+            Boolean Found = false;
+            //Create boolean to assume data is found
+            Boolean OK = true;
+            //Create some test data
+            string Username = "Alfie";
+            string Password = "Password123";
+            //invoke method
+            Found = AnUser.FindUser(Username, Password);
+            //check the userID property
+            if (AnUser.Username != Username && AnUser.Password != Password)
+            {
+                OK = false;            
+            }
+            Assert.IsTrue(OK);
+
+        }
+    }
+}
