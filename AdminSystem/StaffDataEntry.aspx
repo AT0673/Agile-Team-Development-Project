15604cac2180fed2770e4282a3510e211ea3fb35
P2873673-creator <P2873673@my365.dmu.ac.uk>
2026-06-02 03:39:45 +0100

    Added additional class and updates to Staff

--- AdminSystem/StaffDataEntry.aspx
+++ AdminSystem/StaffDataEntry.aspx
@@ -6,10 +6,39 @@
 <head runat="server">
     <title></title>
 </head>
-<body>
+<body
+    style="height: 403px">
     <form id="form1" runat="server">
         <div>
+            <asp:Label ID="lblError" runat="server" style="z-index: 1; left: 8px; top: 359px; position: absolute; bottom: 158px" width="75px"></asp:Label>
+            This is the Staff Data Entry Page<br />
         </div>
+        <asp:Label ID="lblStaffID" runat="server" style="z-index: 1; left: 10px; top: 53px; position: absolute" Text="Staff ID" width="75px"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged" style="z-index: 1; left: 98px; top: 52px; position: absolute"></asp:TextBox>
+        <asp:Button ID="btnOK" runat="server" style="z-index: 1; left: 8px; top: 398px; position: absolute; width: 52px; height: 28px;" Text="OK" OnClick="btnOK_Click" />
+        <asp:Label ID="lblStaffUsername" runat="server" style="z-index: 1; left: 13px; top: 90px; position: absolute" Text="Username" width="75px"></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server" style="z-index: 1; left: 97px; top: 88px; position: absolute" width="128px"></asp:TextBox>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:TextBox ID="TextBox3" runat="server" style="z-index: 1; left: 100px; top: 126px; position: absolute" width="128px"></asp:TextBox>
+        </p>
+        <asp:Label ID="lblStaffPassword" runat="server" style="z-index: 1; left: 10px; top: 129px; position: absolute" Text="Password" width="75px"></asp:Label>
+        <p style="height: 39px">
+            <asp:Label ID="lblStaffLastName" runat="server" style="z-index: 1; left: 13px; top: 173px; position: absolute; height: 21px" Text="Last Name" width="75px"></asp:Label>
+&nbsp;&nbsp;
+        </p>
+        <p style="height: 39px">
+            <asp:TextBox ID="TextBox4" runat="server" style="z-index: 1; left: 99px; top: 172px; position: absolute" width="128px"></asp:TextBox>
+            <asp:Label ID="lblStaffAddress" runat="server" style="z-index: 1; left: 10px; top: 225px; position: absolute; height: 19px; bottom: 292px;" Text="Address" width="75px"></asp:Label>
+            <asp:TextBox ID="TextBox5" runat="server" style="z-index: 1; left: 95px; top: 222px; position: absolute; height: 22px;" width="128px"></asp:TextBox>
+        </p>
+        <p style="height: 32px">
+            <asp:Label ID="lblStaffDate" runat="server" style="z-index: 1; left: 10px; top: 283px; position: absolute" Text="Date Added"></asp:Label>
+            <asp:TextBox ID="TextBox6" runat="server" style="z-index: 1; left: 99px; top: 281px; position: absolute" width="128px"></asp:TextBox>
+        </p>
+        <asp:CheckBox ID="chkActive" runat="server" style="z-index: 1; left: 97px; top: 333px; position: absolute" Text="Active" />
+        <asp:Button ID="btnCancel" runat="server" style="z-index: 1; left: 108px; top: 397px; position: absolute; height: 30px" Text="Cancel" OnClick="btnCancel_Click" width="52px" />
     </form>
 </body>
 </html>
---
+++ ClassLibrary/clsStaffCollection.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class clsStaffCollection
+    {
+           //private data member for the list
+            List<clsStaff> mStaffList = new List<clsStaff>();
+
+            public List<clsStaff> StaffList
+            {
+                get
+                {
+                    return mStaffList;
+                }
+                set
+                {
+                    mStaffList = value;
+                }
+            }
+
+            //private member data for ThisStaff
+            clsStaff mThisStaff = new clsStaff();
+
+            public clsStaff ThisStaff
+            {
+                get
+                {
+                    return mThisStaff;
+                }
+                set
+                {
+                    mThisStaff = value;
+                }
+            }
+
+            //count property
+            public int Count
+            {
+                get
+                {
+                    return mStaffList.Count;
+                }
+                set
+                {
+                    //left blank intentionally
+                }
+            }
+
+            //constructor
+            public clsStaffCollection()
+            {
+                clsDataConnection DB = new clsDataConnection();
+
+                //execute stored procedure
+                DB.Execute("sproc_tblStaff_SelectAll");
+
+                //populate list
+                PopulateArray(DB);
+            }
+
+            //add method
+            public int Add()
+            {
+                clsDataConnection DB = new clsDataConnection();
+
+                DB.AddParameter("@StaffLastName", mThisStaff.StaffLastName);
+                DB.AddParameter("@StaffIsActive", mThisStaff.StaffIsActive);
+                DB.AddParameter("@StaffUsername", mThisStaff.StaffUsername);
+                DB.AddParameter("@StaffPassword", mThisStaff.StaffPassword);
+                DB.AddParameter("@StaffAddress", mThisStaff.StaffAddress);
+                DB.AddParameter("@DateAdded", mThisStaff.DateAdded);
+
+                return DB.Execute("sproc_tblStaff_Insert");
+            }
+
+            //update method
+            public void Update()
+            {
+                clsDataConnection DB = new clsDataConnection();
+
+                DB.AddParameter("@StaffID", mThisStaff.StaffID);
+                DB.AddParameter("@StaffLastName", mThisStaff.StaffLastName);
+                DB.AddParameter("@StaffIsActive", mThisStaff.StaffIsActive);
+                DB.AddParameter("@StaffUsername", mThisStaff.StaffUsername);
+                DB.AddParameter("@StaffPassword", mThisStaff.StaffPassword);
+                DB.AddParameter("@StaffAddress", mThisStaff.StaffAddress);
+                DB.AddParameter("@DateAdded", mThisStaff.DateAdded);
+
+                DB.Execute("sproc_tblStaff_Update");
+            }
+
+            //delete method
+            public void Delete()
+            {
+                clsDataConnection DB = new clsDataConnection();
+
+                DB.AddParameter("@StaffID", mThisStaff.StaffID);
+
+                DB.Execute("sproc_tblStaff_Delete");
+            }
+
+            //filter by surname
+            public void ReportByStaffLastName(string StaffLastName)
+            {
+                clsDataConnection DB = new clsDataConnection();
+
+                DB.AddParameter("@StaffLastName", StaffLastName);
+
+                DB.Execute("sproc_tblStaff_FilterByLastName");
+
+                PopulateArray(DB);
+            }
+
+            //populate list from database
+            private void PopulateArray(clsDataConnection DB)
+            {
+                Int32 Index = 0;
+                Int32 RecordCount = DB.Count;
+
+                mStaffList = new List<clsStaff>();
+
+                while (Index < RecordCount)
+                {
+                    clsStaff AStaff = new clsStaff();
+
+                    AStaff.StaffID = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffID"]);
+                    AStaff.StaffLastName = Convert.ToString(DB.DataTable.Rows[Index]["StaffLastName"]);
+                    AStaff.StaffIsActive = Convert.ToBoolean(DB.DataTable.Rows[Index]["StaffIsActive"]);
+                    AStaff.StaffUsername = Convert.ToString(DB.DataTable.Rows[Index]["StaffUsername"]);
+                    AStaff.StaffPassword = Convert.ToString(DB.DataTable.Rows[Index]["StaffPassword"]);
+                    AStaff.StaffAddress = Convert.ToString(DB.DataTable.Rows[Index]["StaffAddress"]);
+                    AStaff.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateAdded"]);
+
+                    mStaffList.Add(AStaff);
+
+                    Index++;
+
+            }
+        }
+    }
+}
+
+
+
+
